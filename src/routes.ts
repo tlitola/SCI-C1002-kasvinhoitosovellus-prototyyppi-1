1802f4e93fca47d24a952cca6aba09662f8d5ec3
Litola Touko <touko.litola@aalto.fi>
2026-02-15 16:45:29 +0200

    Add initial prototype for the application

--- src/routes.ts
+++ src/routes.ts
@@ -3,12 +3,32 @@ export const routes = {
     title: "Valitse talous",
     screen: "select-household.html",
   },
-  "household-overview": {
-    title: "Talousnäkymä",
-    screen: "household-overview.html",
+  "household-watered": {
+    title: "Dipoli",
+    screen: "household-watered.html",
   },
-  "plant-overview": {
-    title: "Kasvinäkymä",
-    screen: "plant-overview.html",
+  "household-attention": {
+    title: "Dipoli",
+    screen: "household-attention.html",
+  },
+  "household-happy": {
+    title: "221B Baker Street",
+    screen: "household-happy.html",
+  },
+  "plant-needs-watering": {
+    title: "Kasvi – Kastelu tarpeen",
+    screen: "plant-needs-watering.html",
+  },
+  "plant-watered": {
+    title: "Kasvi – Kasteltu",
+    screen: "plant-watered.html",
+  },
+  "plant-healthy": {
+    title: "Kasvi – Kunnossa",
+    screen: "plant-healthy.html",
+  },
+  "plant-healthy-baker-street": {
+    title: "Kasvi – Kunnossa",
+    screen: "plant-healthy-baker-street.html",
   },
 } as const;
